6c57ea7db176c25dc49da179d31a2351f786d3b0
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-05-16 00:57:53 -0500

    Base

---
+++ app/src/main/java/com/dmv/astroid/MainLobbyActivity.java
@@ -0,0 +1,32 @@
+package com.dmv.astroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainLobbyActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_lobby);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainLobbyActivity.this, MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
